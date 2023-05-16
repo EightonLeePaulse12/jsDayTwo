c5b43ba0273cde89fd1d54296f2a14f9de95c077
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2023-05-16 13:54:22 +0200

    Finished

---
+++ js/code.js
@@ -0,0 +1,8 @@
+let subjects = ['Python', 'C++', 'VB.Net', 'Javascript', 'Assembly'];
+
+let lastValue = subjects.length;
+console.log(lastValue)
+
+console.log(subjects[lastValue - 1])
+
+console.log(subjects[lastValue - 3])
